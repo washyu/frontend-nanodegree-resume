ddb2a202e861121bdc7e8207c36141c7a1f8b0cc
washyu <washyu@hotmail.com>
2015-01-07 21:19:26 -0800

    Added header info to page.

---
+++ js/resumeBuilder.js
@@ -0,0 +1,7 @@
+var name = "Shaun Jackson";
+var role = "Web Developer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
